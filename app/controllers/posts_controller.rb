d221bb679946ec96472b4acfbbc83ff257c68427
Josh Pann <josh.pann@example.com>
2016-12-30 17:45:47 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
+###these are actions as defined by rails helper "resources" in the routes.rb file
   def index
     @posts = Post.all
   end
-  
+
+  def new
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+    ###posts_path = get request to /posts
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
+
+  ###Rails creates routes through the resources helper
+# Prefix   Verb URI Pattern          Controller#Action
+#  posts   GET  /posts(.:format)     posts#index
+#          POST /posts(.:format)     posts#create
+# new_post GET  /posts/new(.:format) posts#new
 end
